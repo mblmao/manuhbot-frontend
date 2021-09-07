73d3e9c64213b5eebb27a8ca2f73ed0fce794d46
mblmao <65665790+mblmao@users.noreply.github.com>
2021-09-07 17:13:37 -0300

    Update Chat.js
    Moving to online backend: https://backend-manuhbot.herokuapp.com/

--- src/Chat.js
+++ src/Chat.js
@@ -12,7 +12,7 @@ const Chat = () => {
     }
 
     axios
-      .post('http://localhost:3001/chatbot', data)
+      .post('https://backend-manuhbot.herokuapp.com/chatbot', data)
       .then((response) => {
         const responseData = {
           text:
